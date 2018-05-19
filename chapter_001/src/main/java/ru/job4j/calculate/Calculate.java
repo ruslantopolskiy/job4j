36fafed1225253fe685efd06fbbc32d0bfc484de
ruslantopolskiy <ruslan.topolskiy@gmail.com>
2018-05-19 21:41:56 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,18 @@
-package ru.job4j.calculate;
+package ru.job4j;
+
+/**
+* @author ruslan topolskiy
+* @since 19.05.2018
+* @version 1.0
+*/
 
 public class Calculate {
+
+/**
+* Metod Main
+* @param args
+*/
+
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
