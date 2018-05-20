6c1d8421a361dba56954172d33545a01315946a9
ruslantopolskiy <ruslan.topolskiy@gmail.com>
2018-05-20 04:42:19 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -12,8 +12,16 @@ public class Calculate {
 * Metod Main
 * @param args
 */
-
     public static void main(String[] args) {
         System.out.println("Hello World");
+    }
+
+   /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
     }
 }
