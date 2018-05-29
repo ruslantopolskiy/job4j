01eb89da8e107f8a4af614e163941a99e2fc3daf
ruslantopolskiy <ruslan.topolskiy@gmail.com>
2018-05-29 16:07:45 +0300

    5.4. Слова начинается с ... [#55305]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,34 @@
+package ru.job4j.array;
+
+/**
+ * Обертка над строкой.
+ *
+ * @author Ruslan Topolskiy(ruslan.topolskiy@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ArrayChar {
+    private char[] data;
+
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+
+    /**
+     * Проверяет. что слово начинается с префикса.
+     *
+     * @param prefix префикс.
+     * @return если слово начинаеться с префикса
+     */
+    public boolean startWith(String prefix) {
+        boolean result = true;
+        char[] value = prefix.toCharArray();
+        for (int i = 0; i < value.length; i++) {
+            if (data[i] != value[i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
