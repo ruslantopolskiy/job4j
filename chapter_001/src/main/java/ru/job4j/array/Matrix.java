43d9c18a753df4d43beb87bfa3fdbe421f2c4dc8
ruslantopolskiy <ruslan.topolskiy@gmail.com>
2018-05-31 21:42:44 +0300

    5.6. Двухмерный массив. Таблица умножения. [#33491]

---
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+/**
+ * Двухмерный массив. Таблица умножения.
+ *
+ * @author Ruslan Topolskiy(ruslan.topolskiy@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Matrix {
+
+    /**
+     * Metod multiple
+     *
+     * @param size
+     * @return
+     */
+    public int[][] multiple(int size) {
+        int table[][] = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                table[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return table;
+    }
+}
