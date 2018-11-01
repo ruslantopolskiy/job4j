14248a9916b3fdee870929b4ec04138ea06f9470
ruslantopolskiy <ruslan.topolskiy@gmail.com>
2018-11-01 11:39:45 +0300

    1

---
+++ chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
@@ -0,0 +1,62 @@
+﻿package ru.job4j.tracker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuTracker {
+    private Input input;
+    private Tracker tracker;
+
+    public MenuTracker(Input input, Tracker tracker) {
+        this.input = input;
+        this.tracker = tracker;
+    }
+
+    private List<UserAction> actions = new ArrayList<>();
+
+    public void fillActions() {
+        actions.add(0, new AddItem());
+    }
+
+    public int getActionSize() {
+        return actions.size();
+    }
+
+    public void select(int key) {
+        actions.get(key).execute(this.input, this.tracker);
+    }
+
+    public void show() {
+        for (UserAction action : this.actions) {
+            if (action != null) {
+                System.out.println(action.info());
+            }
+        }
+    }
+
+    public class AddItem implements UserAction {
+        @Override
+        public int key() {
+            return 0;
+        }
+
+        @Override
+        public void execute(Input input, Tracker tracker) {
+            System.out.println("------------ Adding new item --------------");
+            String name = input.ask("Please, provide item name:");
+            String description = input.ask("Please, provide item description:");
+            Item item = new Item(name, description);
+            tracker.add(item);
+            System.out.println("------------ New Item with Id : " + item.getId());
+            System.out.println("------------ New Item with Name : " + item.getName());
+            System.out.println("------------ New Item with Description : " + item.getDescription());
+        }
+
+        @Override
+        public String info() {
+            return "Add new Item.";
+        }
+    }
+
+
+}
