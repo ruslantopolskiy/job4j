216a037fd26b6b106efe57bf81bd648f59659a75
topolskiy <ruslan.topolskiy@gmail.com>
2018-12-25 17:17:04 +0300

    2. Сортировка User с использованием Comparator [#10036]

--- chapter_003/src/test/java/ru/job4j/Comparable/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/Comparable/SortUserTest.java
@@ -2,10 +2,7 @@ package ru.job4j.Comparable;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -16,12 +13,29 @@ public class SortUserTest {
         SortUser sortUser = new SortUser();
         List<User> list = Arrays.asList(
                 new User("Руслан", 2),
-                new User("Вася", 10),
+                new User("Вася", 44),
                 new User("Вадим", 22));
         Set<User> set = new TreeSet<>();
         set.add(new User("Руслан", 2));
-        set.add(new User("Вася", 10));
         set.add(new User("Вадим", 22));
+        set.add(new User("Вася", 44));
         assertThat(sortUser.sort(list), is(set));
     }
+
+    @Test
+    public void whenAgeThenSortNameAge() {
+        SortUser sortUser = new SortUser();
+        List<User> list = Arrays.asList(
+                new User("Руслан", 2),
+                new User("Алекc", 44),
+                new User("Алекc", 22));
+                new User("Яков",40);
+        List<User> set = new ArrayList<>();
+        set.add(new User("Алекс", 22));
+        set.add(new User("Алекся", 44));
+        set.add(new User("Руслан",2));
+        set.add(new User("Яков", 40));
+        assertThat(sortUser.sortByAllFields(list), is(set));
+    }
+
 }
