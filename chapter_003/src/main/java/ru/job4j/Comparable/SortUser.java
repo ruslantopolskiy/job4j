724e836c972e098f776085d1c2945caaf473f100
topolskiy <ruslan.topolskiy@gmail.com>
2018-12-25 19:43:58 +0300

    2. Сортировка User с использованием Comparator [#10036]

--- chapter_003/src/main/java/ru/job4j/Comparable/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/Comparable/SortUser.java
@@ -22,7 +22,7 @@ public class SortUser {
         list.sort(new Comparator<User>() {
             @Override
             public int compare(User o1, User o2) {
-                Integer result =  o1.getName().compareTo(o2.getName());
+                int result =  o1.getName().compareTo(o2.getName());
                 if (result == 0){
                     result = o1.getAge()- o2.getAge();
                 }
