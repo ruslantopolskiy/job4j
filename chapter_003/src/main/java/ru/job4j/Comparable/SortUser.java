cb0098685530a687b8a5c0d73378e5241a179303
topolskiy <ruslan.topolskiy@gmail.com>
2019-01-01 18:47:28 +0300

    Исправление ошибок.

--- chapter_003/src/main/java/ru/job4j/Comparable/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/Comparable/SortUser.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class SortUser {
     public Set<User> sort(List<User> list) {
-        return new TreeSet <>(list);
+        return new TreeSet<>(list);
     }
 
     public List<User> sortNameLength(List<User> list) {
@@ -18,17 +18,17 @@ public class SortUser {
         return list;
     }
 
-    public List<User> sortByAllFields (List<User> list){
+    public List<User> sortByAllFields(List<User> list) {
         list.sort(new Comparator<User>() {
             @Override
             public int compare(User o1, User o2) {
-                int result =  o1.getName().compareTo(o2.getName());
-                if (result == 0){
-                    result = o1.getAge()- o2.getAge();
+                int result = o1.getName().compareTo(o2.getName());
+                if (result == 0) {
+                    result = o1.getAge() - o2.getAge();
                 }
                 return result;
             }
         });
-            return list;
+        return list;
     }
 }
--- chapter_003/src/main/java/ru/job4j/Comparable/User.java
+++ chapter_003/src/main/java/ru/job4j/Comparable/User.java
@@ -2,11 +2,11 @@ package ru.job4j.comparable;
 
 import java.util.Objects;
 
-public class User implements Comparable <User> {
+public class User implements Comparable<User> {
     private String name;
     private Integer age;
 
-    public User (String name, Integer age){
+    public User(String name, Integer age) {
         this.name = name;
         this.age = age;
     }
@@ -22,19 +22,18 @@ public class User implements Comparable <User> {
 
     @Override
     public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
+        return "User{" + "name='" + name + '\'' + ", age=" + age + '}';
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        } else if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         User user = (User) o;
-        return Objects.equals(name, user.name) &&
-                Objects.equals(age, user.age);
+        return Objects.equals(name, user.name) && Objects.equals(age, user.age);
     }
 
     @Override
