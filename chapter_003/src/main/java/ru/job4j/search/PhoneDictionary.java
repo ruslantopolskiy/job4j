c504337ef0394e760a471426af95ce1e1fd7fa70
topolskiy <ruslan.topolskiy@gmail.com>
2019-01-10 17:49:52 +0300

    1. Телефонный справочник на базе ArrayList [#41598]

---
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,30 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhoneDictionary {
+    private List<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     *
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+    public List<Person> find(String key) {
+        List<Person> result = new ArrayList<>();
+        for (var index = 0; index < persons.size(); index++)
+            if (persons.get(index).getAddress().contains(key)
+                    || persons.get(index).getName().contains(key)
+                    || persons.get(index).getPhone().contains(key)
+                    || persons.get(index).getSurname().contains(key)) {
+                result.add(persons.get(index));
+            }
+            return result;
+    }
+}
