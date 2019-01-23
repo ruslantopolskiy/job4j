acb08d862427ffec07d48675351bc6ac48da1484
topolskiy <ruslan.topolskiy@gmail.com>
2019-01-23 12:11:12 +0300

    commit squas

--- chapter_004/src/main/java/ru/job4j/lambda/Diapason.java
+++ chapter_004/src/main/java/ru/job4j/lambda/Diapason.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
-public class Diapasons {
+public class Diapason {
     public List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> result = new ArrayList<>();
         for (int index = start; index < end; index++) {
--- chapter_004/src/test/java/ru/job4j/lambda/DiaposonTest.java
+++ chapter_004/src/test/java/ru/job4j/lambda/DiaposonTest.java
@@ -9,11 +9,11 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 
-public class DiapasonTest {
+public class DiaposonTest {
 
     @Test
     public void whenLinearFunctionThenLinearResults() {
-        Diapasons diapasons = new Diapasons();
+        Diapason diapasons = new Diapason();
         List<Double> result = diapasons.diapason(5, 8, x -> 2 * x + 1);
         List<Double> expected = Arrays.asList(11D, 13D, 15D);
         assertThat(result, is(expected));
@@ -21,7 +21,7 @@ public class DiapasonTest {
 
     @Test
     public void whenLinearFunctionThenQuadroResults() {
-        Diapasons diapasons = new Diapasons();
+        Diapason diapasons = new Diapason();
         List<Double> result = diapasons.diapason(5, 8, x -> x * x + 1);
         List<Double> expected = Arrays.asList(26D, 37D, 50D);
         assertThat(result, is(expected));
@@ -29,7 +29,7 @@ public class DiapasonTest {
 
     @Test
     public void whenLinearFunctionThenLogResults() {
-        Diapasons diapasons = new Diapasons();
+        Diapason diapasons = new Diapason();
         List<Double> result = diapasons.diapason(1, 4, x -> Math.log(x) + 1);
         List<Double> expected = Arrays.asList(1D, 1.6931471805599454D, 2.09861228866811D);
         assertThat(result, is(expected));
