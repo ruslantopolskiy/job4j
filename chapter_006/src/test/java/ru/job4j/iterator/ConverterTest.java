9fcc97c33f5956930adc1dddb2e8f1960b54ca3e
topolskiy <ruslan.topolskiy@gmail.com>
2019-01-23 23:55:45 +0300

    5.1.4. Создать convert(Iterator<Iterator>) [#152] #2

--- chapter_006/src/test/java/ru/job4j/iterator/ConverterTest.java
+++ chapter_006/src/test/java/ru/job4j/iterator/ConverterTest.java
@@ -98,5 +98,19 @@ public class ConverterTest {
         assertThat(it.next(), is(3));
         it.next();
     }
+    @Test
+    public void hasNextShouldReturnFalseInCaseOfEmptyIterators2() {
+        Iterator<Integer> it1 = (new ArrayList<Integer>()).iterator();
+        Iterator<Integer> it2 = (new ArrayList<Integer>()).iterator();
+        Iterator<Integer> it3 = (Arrays.asList(1)).iterator();
+        Iterator<Iterator<Integer>> its = Arrays.asList(it1, it2, it3).iterator();
+        Converter iteratorOfIterators = new Converter();
+        it = iteratorOfIterators.convert(its);
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+
+    }
+
+
 }
 
