425307c0f36c83f250835bf194e85e016edebb71
topolskiy <ruslan.topolskiy@gmail.com>
2019-02-07 18:58:49 +0300

    5.3.4. Задан связанный список. Определить цикличность. [#161] #2

--- chapter_006/src/main/java/ru/job4j/set/SimpleSet.java
+++ chapter_006/src/main/java/ru/job4j/set/SimpleSet.java
@@ -1,18 +1,37 @@
 package ru.job4j.set;
 
-        import ru.job4j.list.DynamicMassive;
+import ru.job4j.list.DynamicMassive;
 
-        import java.util.Iterator;
+import java.util.Iterator;
 
 public class SimpleSet<T> implements Iterable<T> {
     private DynamicMassive dynamicMassive;
+    private int size;
+    private boolean result;
 
     public SimpleSet() {
         this.dynamicMassive = new DynamicMassive();
     }
 
     public void add(T value) {
-        dynamicMassive.add(value);
+        if (uniqueValue(value)) {
+            dynamicMassive.add(value);
+        }
+    }
+
+    public int sizeMassive() {
+        return this.size = dynamicMassive.getSize();
+    }
+
+
+    public boolean uniqueValue(T value) {
+        boolean result = true;
+        for (int index = 0; index < dynamicMassive.getSize(); index++) {
+            if (dynamicMassive.get(index).equals(value)) {
+                result = false;
+            }
+        }
+        return result;
     }
 
     @Override
---
+++ chapter_006/src/test/java/ru/job4j/set/SimpleSetTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.set;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SimpleSetTest {
+    SimpleSet <Integer> simpleSet;
+
+    @Before
+    public void before() {
+        simpleSet = new SimpleSet<>();
+        simpleSet.add(1);
+        simpleSet.add(1);
+        simpleSet.add(2);
+        simpleSet.add(2);
+        simpleSet.add(3);
+    }
+    @Test
+    public void WhenAdd(){
+        assertThat(simpleSet.sizeMassive(),is(2));
+    }
+
+
+}
