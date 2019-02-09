da6f5a95221b76a13bbe1b74bdba6c1de889f8ae
topolskiy <ruslan.topolskiy@gmail.com>
2019-02-10 00:55:58 +0300

    1. Создать модель User [#999]

---
+++ chapter_006/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,40 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+}
+
