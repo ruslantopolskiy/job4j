fcebfb1adff383851c840ca18fad65a71ee45cae
topolskiy <ruslan.topolskiy@gmail.com>
2019-02-11 17:08:57 +0300

    3. Переопределить только hashCode [#1003]

--- chapter_006/src/main/java/ru/job4j/map/MapUser.java
+++ chapter_006/src/main/java/ru/job4j/map/MapUser.java
@@ -1,12 +1,29 @@
 package ru.job4j.map;
 
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class MapUser {
+    User first = new User("Oleg", 21, new GregorianCalendar(1990, 9, 21));
+    User second = new User("Oleg", 21, new GregorianCalendar(1990, 9, 21));
+    Map<User, Object> map = new HashMap<>();
+
+    public void print() {
+        map.put(first, (int) 1);
+        map.put(second, (int) 2);
+        System.out.println(map);
+        System.out.println(first.equals(second));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second, map);
+    }
 
     public static void main(String[] args) {
-        User first = new User("Oleg", 21, new GregorianCalendar(1990, 9, 21));
-        User second = new User("Oleg", 21, new GregorianCalendar(1990, 9, 21));
-        System.out.println(first.equals(second));
+        MapUser mapUser = new MapUser();
+        mapUser.print();
     }
 }
